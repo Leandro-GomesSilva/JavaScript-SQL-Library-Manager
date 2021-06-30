88b729ab30797a3c546d0fd3fbb0b9c7a8e9a247
Le-Gomes <69648505+Le-Gomes@users.noreply.github.com>
2021-06-30 22:48:20 +0200

    Book model
    Testing the Book model and its communication to the database.

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 // Importing the Book model
-const Book = require('../models/book')
+const {Book} = require('../models')
 
 /* GET home page. */
 router.get('/', async (req, res, next) => { 
   const books = await Book.findAll();   // Storing all books from the Model into a variable
+  res.json(books);  // Sending a JSON response
 });
 
 module.exports = router;
